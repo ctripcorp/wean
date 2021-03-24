edd921f33da3f8e042ef8a669464dc4c620198fd
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-03-24 19:52:10 +0800

    refactor

--- core/cli.js
+++ core/cli.js
@@ -1,15 +1,51 @@
+const chokidar = require("chokidar")
+const chalk = require("chalk")
+const Path = require("path")
+const build = require("./bundle")
+const pack = require("./package")
+const serve = require("./serve")
+const argv = require("./commander")
 
-const argv = require('./commander')
-const chalk = require('chalk')
-
-const cmd = cmdMap.get(argv.type);
-
-if (cmd) {
-  cmd();
-} else {
-  console.log(
-    chalk.yellow(`
-    No commander is specified.
-  `),
-  );
+async function run(b) {
+  const options = {
+    e: "./app.json",
+    o: "./dist/",
+    i: "/",
+    w: argv.watch,
+    e: argv.entry,
+    o: argv.output,
+    p: argv.publicUrl,
+    b,
+  }
+  start(options)
+  if (options.w) {
+    chokidar
+      .watch(Path.dirname(options.e), {
+        ignored: /(dist|.git)/,
+        persistent: true,
+        awaitWriteFinish: {
+          stabilityThreshold: 500,
+          pollInterval: 500,
+        },
+      })
+      .on("change", (path) => {
+        console.log(chalk.green(`rebuild ${path}`))
+        start(options)
+      })
+  }
 }
+
+async function start(options) {
+  options.old && options.old.close()
+  const adt = await build(options.e, options)
+  console.log(chalk.green("bundle success"))
+  await pack(adt, options)
+  console.log(chalk.green("package success"))
+  if (!options.b) {
+    options.old = serve(options)
+  } else {
+    console.log(chalk.green("build success"))
+  }
+}
+
+run(argv.b)
