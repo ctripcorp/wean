a01a1c79e52c25f3ef492a0634d0e6e7efc9b7d7
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-03-29 18:15:31 +0800

    递归处理

--- core/packagers/js.js
+++ core/packagers/js.js
@@ -4,19 +4,25 @@ module.exports = async function packJs(asset, options) {
   const defer = []
   const cache = []
   asset.output = asset.code
-  for (const dep of asset.childAssets.values()) {
-    if (dep.tag) {
-      defer.push(dep.code)
-    } else {
-      if (cache.indexOf(dep.path) < 0) {
-        asset.output = dep.code + "\n" + asset.output
-        cache.push(dep.path)
+  const walk = async (child) => {
+    for (const dep of child.childAssets.values()) {
+      if (dep.tag) {
+        defer.push(dep.code)
+      } else {
+        if (cache.indexOf(dep.path) < 0) {
+          asset.output = dep.code + "\n" + asset.output
+          cache.push(dep.path)
+        }
+      }
+      if (dep.childAssets.size) {
+        await walk(dep)
       }
     }
-  }
 
-  for (const code of defer) {
-    asset.output += "\n" + code
+    for (const code of defer) {
+      asset.output += "\n" + code
+    }
   }
+  walk(asset)
   return asset.output + "\n\n"
 }
--- core/packagers/wxml.js
+++ core/packagers/wxml.js
@@ -8,12 +8,18 @@ module.exports = async function packWxml(asset, options) {
       ? `const $${asset.parent.id}`
       : `remotes['${titleCase(asset.parent.tag)}']`
   asset.output = `${name} = ${asset.code}\n\n`
-  for (const dep of asset.childAssets.values()) {
-    let code = `remotes['${dep.id}'] = ${dep.code}\n\n`
-    if (cache.indexOf(dep.path) < 0) {
-      asset.output += code
-      cache.push(dep.path)
+  const walk = async (child) => {
+    for (const dep of child.childAssets.values()) {
+      let code = `remotes['${dep.id}'] = ${dep.code}\n\n`
+      if (cache.indexOf(dep.path) < 0) {
+        asset.output += code
+        cache.push(dep.path)
+      }
+      if (dep.childAssets.size) {
+        await walk(dep)
+      }
     }
   }
+  walk(asset)
   return asset.output
 }
