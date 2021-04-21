9455b023def591d6be9b8d7525a329238a41c300
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-04-21 16:00:42 +0800

    构建作用域链

--- core/visitors/index.js
+++ core/visitors/index.js
@@ -1,7 +1,52 @@
+const Scope = require("./scope")
+const traverse = require("./traverse")
+
 function analyse(ast, magicString, module) {
+  let scope = new Scope()
+
   ast.body.forEach((statement) => {
-    // 待补充
+    const addToScope = (declaration) => {
+      let name = declaration.id.name
+      scope.add(name)
+      if (!scope.parent) {
+        statement._defines[name] = true
+      }
+    }
+
     statement._source = magicString.snip(statement.start, statement.end)
+    statement._defines = {}
+    statement._includes = {}
+    statement._depends = {}
+
+    traverse(statement, {
+      enter(node) {
+        let newScope
+        switch (node.type) {
+          case "FunctionDeclaration":
+            const params = node.params.map((p) => p.name)
+            addToScope(node)
+            newScope = new Scope({
+              parent: scope,
+              params,
+            })
+            break
+          case "VariableDeclaration":
+            node.declarations.forEach(addToScope)
+            break
+          default:
+            break
+        }
+        if (newScope) {
+          node._scope = newScope
+          scope = newScope
+        }
+      },
+      lerve(node) {
+        if (node._scope) {
+          scope = scope.parent
+        }
+      },
+    })
   })
 }
 module.exports = analyse
