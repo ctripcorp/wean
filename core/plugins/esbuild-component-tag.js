93d531db442b97a9f3add66b14e5678a1639a146
伊撒尔 <changhaozhao@trip.com>
2021-06-30 12:22:31 +0800

    remove ast ops (#73)

--- core/plugins/esbuild-component-tag.js
+++ core/plugins/esbuild-component-tag.js
@@ -6,11 +6,10 @@ module.exports = options => {
         setup(build) {
             build.onLoad({ filter: /.+/ }, async ({ path }) => {
                 const source = await fs.promises.readFile(path, "utf8")
-                const code = source.
-                    replace('Component(', `Component('${options.id}','${options.tag}','${options.pid}', `)
-                    .replace('Page(', `Page('${options.id}', `)
+                const pagecode = `\nPage.id="${options.id}"\n` + source
+                const componentcode = `\nComponent.id="${options.id}"\nComponent.pid="${options.pid}"\nComponent.tag="${options.tag || ''}"\n` + source
                 return {
-                    contents: code,
+                    contents: options.tag ? componentcode : pagecode,
                 }
             })
         }
